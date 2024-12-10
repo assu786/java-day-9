f0303f505a3f6fbf8bef460200bf0a6615ccda47
assu786 <shaikaqsa.78@gmail.com>
2024-12-10 12:26:38 +0530

    Add files via upload

---
+++ splitstring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class A 
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in);
+    String a=s.nextLine();
+    String sp[]=a.split(" ");
+    System.out.println(Arrays.toString(sp));
+}
+}

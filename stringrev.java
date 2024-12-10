30c0eb6315693bf99a3363a847c2a5c9090a86c5
assu786 <shaikaqsa.78@gmail.com>
2024-12-10 12:36:56 +0530

    Add files via upload

---
+++ stringrev.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.next();
+        System.out.println(a);
+        String b=s.nextLine();
+        System.out.println(b);
+    }
+}
